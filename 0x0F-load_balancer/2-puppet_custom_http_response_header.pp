70a2abbe80575c309e5f46e8598c0b57f4cc65c8
Crisgrva <crstngranada@gmail.com>
2022-01-11 16:30:52 -0500

    [ Task 2 ] Add puppet script that setup custom http header [ 01/11/2022/04:30 ]

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,12 @@
+# 2. Add a custom HTTP header with Puppet
+exec { 'HTTP_header_Puppet':
+  command  => 'sudo apt-get -y update;
+               sudo apt-get -y install nginx;
+               echo "Hello World!" > /var/www/html/index.nginx-debian.html;
+               service nginx start;
+               sudo chmod 777 /etc/nginx;
+               sudo chmod 777 /etc/nginx/nginx.conf;
+               sudo sed -i '/sendfile on;/ a add_header X-Served-By $HOSTNAME;' /etc/nginx/nginx.conf;
+               sudo service nginx restart',
+  provider => 'shell',
+}
